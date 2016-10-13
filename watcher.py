6a5a233355b122d60a346190c04842ce9bccabf9
Ruslan Gustomyasov <rusik@yandex-team.ru>
2016-10-13 23:01:26 +0300

    commit

--- watcher.py
+++ watcher.py
@@ -6,6 +6,7 @@ import docker
 import logging
 import sys
 import time
+import ipdb
 
 logging.basicConfig(filename=settings.watcher_log, level=settings.log_level, format=u'%(asctime)s  %(message)s', )
 ch = logging.StreamHandler(sys.stdout)
@@ -56,16 +57,16 @@ class Watcher:
         for pod_path, pod_json in self.pods_list.items():
             if self.get_host_name(pod_path) == pod:
                 pod_json_json = json.loads(pod_json)
-                pod_json_json['containers_list'] += container_id
+                pod_json_json['containers_list'] .append(str(container_id))
                 self.pods_list[pod_path] = json.dumps(pod_json_json)
-        for host_path, host_json in self.host_list.items():
+        for host_path, host_json in self.hosts_list.items():
             if self.get_host_name(host_path) == host:
                 host_json_json = json.loads(host_json)
-                host_json_json['containers_list'] += container_id
+                host_json_json['containers'].append(str(container_id))
                 self.hosts_list[host_path] = json.dumps(host_json_json)
         return container_id
 
-    def stop_container(self, host, container_id):
+    def stop_container(self, host, port, container_id):
         cli = docker.Client(base_url='tcp://' + host + ':' + port,
                             version=settings.docker_version)
         cli.stop(container_id)
@@ -95,7 +96,7 @@ class Watcher:
         pod_memory = int(pod_json['memory'])
         host_disk = int(host_json['disk'])
         pod_disk = int(host_json['disk'])
-        if host_cpus >= pod_cpus and host_memory > pod_memory and host_disk > pod_disk:
+        if host_cpus >= pod_cpus and host_memory >= pod_memory and host_disk >= pod_disk:
             return True
         return False
 
@@ -116,7 +117,7 @@ class Watcher:
         host_json['cpus'] = new_host_cpus
         host_json['memory'] = new_host_memory
         host_json['disk'] = new_host_disk
-        host_json['containers'] += container_id
+        host_json['containers'].append(str(container_id))
         self.hosts_list[host] = json.dumps(host_json)
 
     def update_host_config_plus(self, host, pod, container_id):
@@ -143,56 +144,51 @@ class Watcher:
         for host_path, host_json_str in self.hosts_list.items():
             host_json_json = json.loads(host_json_str)
             if container_id in host_json_json['containers']:
-                return host_path
-        return False
+                return host_json_json['host'], host_json_json['port']
+        return False, False
 
     def schedule(self):
         for pod_path, pod_json in self.pods_list.items():
             pod_name = self.get_host_name(pod_path)
             pod_json_json = json.loads(pod_json)
             pod_containers = int(pod_json_json['containers'])
-            host_containers = len(pod_json_json['containers_list'])
-            if pod_containers > host_containers:
-                runs = pod_containers - host_containers
-                i = 0
-                while i < runs:
-                    run = False
-                    for host_path, host_json in self.hosts_list.items():
-                        if self.check_if_container_fits_on_host(host_path, pod_path):
-                            host_json_json = json.dumps(host_json)
-                            host = host_json_json['host']
-                            port = host_json_json['port']
-                            image = host_json_json['image']
-                            memory = host_json_json['memory']
-                            container_id = self.run_container(pod_name, host, port, image, memory)
-                            run = True
-                            self.update_host_config_minus(host_path, pod_path, container_id)
-                            runs -= 1
-                            logging.warning('run ' + self.get_host_name(host_path) + ' container')
-                    if not run:
-                        logging.error('unable to run pod ' + pod_name)
-            elif pod_containers < host_containers:
-                runs = host_containers - pod_containers
+            running_containers = len(pod_json_json['containers_list'])
+            if pod_containers > running_containers:
                 run = False
-                while i < runs:
-                    container_id = pod_json_json['containers_list'][0]
-                    container_host = self.find_container_host(container_id)
-                    result = self.stop_container(container_host, container_id)
-                    run = True
+                for host_path, host_json in self.hosts_list.items():
+                    if self.check_if_container_fits_on_host(host_path, pod_path):
+                        host_json_json = json.loads(host_json)
+                        host = host_json_json['host']
+                        port = host_json_json['port']
+                        image = pod_json_json['image']
+                        memory = pod_json_json['memory']
+                        container_id = self.run_container(pod_name, host, port, image, memory)
+                        run = True
+                        self.update_host_config_minus(host_path, pod_path, container_id)
+                        logging.warning('run ' + pod_name + ' container')
+                        break
+                    break
                 if not run:
-                    logging.error('unable to delete pod ' + pod_name)
-
-
+                    logging.error('unable to run pod ' + pod_name + ' on host ' + host)
+            elif pod_containers < running_containers:
+                container_id = pod_json_json['containers_list'][0]
+                container_host, container_port = self.find_container_host(container_id)
+                if container_host != False and container_port != False:
+                    result = self.stop_container(container_host, container_port, container_id)
+                    logging.warning('removed ' + container_id + ' from ' + container_host)
+                break
+            break
 
     def write_all_to_etcd(self):
         logging.warning(self.pods_list)
         logging.warning(self.hosts_list)
         etcd_client = etcd.Client(host=self.etcd_host, port=self.etcd_port)
         for pod_path, pod_json in self.pods_list.items():
-            etcd_client.set(pod_path, pod_json)
+            etcd_client.set(pod_path, json.dumps(pod_json))
         for host_path, host_json in self.hosts_list.items():
-            etcd_client.set(host_path, host_json)
-
+            logging.error(host_path)
+            logging.error(host_json)
+            etcd_client.set(host_path, json.dumps(host_json))
 
     def watch(self):
         while (True):
--- web.py
+++ web.py
@@ -14,42 +14,72 @@ logging.warning('starting web')
 app = Flask(__name__)
 
 
-@app.route('/add_host')
+@app.route('/add_host', methods=['POST'])
+def add_host_api():
+    logging.debug('add_host')
+    host_name = request.json.get('host_name', settings.default_host)
+    done = False
+    try:
+        if not request.json:
+            abort(400)
+        host = request.json.get('host', settings.default_host)
+        port = request.json.get('port', settings.default_port)
+        cpus = request.json.get('cpus', settings.default_cpus_host)
+        memory = request.json.get('memory', settings.default_memory_host)
+        disk = request.json.get('disk', settings.default_disk_host)
+        data_list = {}
+        data_list['host'] = host
+        data_list['port'] = port
+        data_list['cpus'] = cpus
+        data_list['memory'] = memory
+        data_list['disk'] = disk
+        data_list['containers'] = []
+        data = json.dumps(data_list)
+        etcd_client = etcd.Client(host=settings.etcd_host, port=settings.etcd_port)
+        etcd_client.write(settings.etcd_prefix + settings.etcd_hosts_prefix + host_name, data)
+        done = True
+        logging.debug('host ' + host_name + ' added')
+        return 'OK'
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        pass
+
+
+@app.route('/add_host', methods=['GET'])
 def add_host():
     try:
         logging.debug('add_host')
         done = False
-        host_name = request.args.get('host_name', '')
-        if (host_name != ''):
-            host = request.args.get('host', settings.default_host)
-            port = request.args.get('port', settings.default_port)
-            cpus = request.args.get('cpus', settings.default_cpus_host)
-            memory = request.args.get('memory', settings.default_memory_host)
-            disk = request.args.get('disk', settings.default_disk_host)
-            data_list = {}
-            data_list['host'] = host
-            data_list['port'] = port
-            data_list['cpus'] = cpus
-            data_list['memory'] = memory
-            data_list['disk'] = disk
-            data_list['containers'] = {}
-            data = json.dumps(data_list)
-            etcd_client = etcd.Client(host=settings.etcd_host, port=settings.etcd_port)
-            etcd_client.write(settings.etcd_prefix + settings.etcd_hosts_prefix + host_name, data)
-            done = True
-            logging.debug('host ' + host_name + ' added')
+        host_name = request.args.get('host_name', settings.default_host)
+        host = request.args.get('host', settings.default_host)
+        port = request.args.get('port', settings.default_port)
+        cpus = request.args.get('cpus', settings.default_cpus_host)
+        memory = request.args.get('memory', settings.default_memory_host)
+        disk = request.args.get('disk', settings.default_disk_host)
+        data_list = {}
+        data_list['host'] = host
+        data_list['port'] = port
+        data_list['cpus'] = cpus
+        data_list['memory'] = memory
+        data_list['disk'] = disk
+        data_list['containers'] = []
+        data = json.dumps(data_list)
+        etcd_client = etcd.Client(host=settings.etcd_host, port=settings.etcd_port)
+        etcd_client.write(settings.etcd_prefix + settings.etcd_hosts_prefix + host_name, data)
+        done = True
+        logging.debug('host ' + host_name + ' added')
         return render_template('add_host.html', done=done)
     except Exception as e:
         logging.error(e, exc_info=True)
         pass
 
 
-@app.route('/add_pod')
+@app.route('/add_pod', methods=['GET'])
 def add_pod():
     try:
         logging.debug('add_pod')
         done = False
-        pod_name = request.args.get('name', '')
+        pod_name = request.args.get('name', settings.default_pod_name)
         if (pod_name != ''):
             image = request.args.get('image', settings.default_image_pod)
             containers = request.args.get('containers', 1)
@@ -75,6 +105,38 @@ def add_pod():
         logging.error(e, exc_info=True)
         pass
 
+
+@app.route('/add_pod', methods=['POST'])
+def add_pod_post():
+    try:
+        logging.debug('add_pod')
+        done = False
+        pod_name = request.json.get('name', settings.default_host)
+        if not request.json:
+            abort(400)
+        image = request.json.get('image', settings.default_image_pod)
+        containers = request.json.get('containers', 1)
+        cpus = request.json.get('cpus', settings.default_cpus_pod)
+        memory = request.json.get('memory', settings.default_memory_pod)
+        disk = request.json.get('disk', settings.default_disk_pod)
+        data_list = {}
+        data_list['image'] = image
+        data_list['containers'] = containers
+        data_list['cpus'] = cpus
+        data_list['memory'] = memory
+        data_list['disk'] = disk
+        data_list['state'] = 'stopped'
+        data_list['running_containers'] = 0
+        data_list['containers_list'] = []
+        data = json.dumps(data_list)
+        etcd_client = etcd.Client(host=settings.etcd_host, port=settings.etcd_port)
+        etcd_client.write(settings.etcd_prefix + settings.etcd_pods_prefix + pod_name, data)
+        done = True
+        logging.debug('pod ' + pod_name + ' added')
+        return render_template('add_pod.html', done=done)
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        pass
 
 @app.route("/", methods=['GET'])
 def index():
